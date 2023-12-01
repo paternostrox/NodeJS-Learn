1fb37d3d10c0f551974329aae8b954902757e7d9
Pedro Paternostro <pedro.paternostro@gmail.com>
2023-12-01 10:56:07 -0300

    Added message history to socketio chat

--- SocketIO-chat/server/index.js
+++ SocketIO-chat/server/index.js
@@ -4,13 +4,21 @@ const io = require('socket.io')(http, {
     cors: { origin: '*'}
 });
 
+let messageHistory = [];
+
 io.on('connection', (socket) => {
     console.log('a user connected');
+    for (let message of messageHistory) {            
+        socket.emit('message', message);
+    }
 
     socket.on('message', (data) => {
         console.log('client sent:', data);
-        io.emit('message', `${socket.id.substr(0, 2)} said ${data}`);
+        const message = `${socket.id.substr(0, 2)} said ${data}`;
+        messageHistory.push(message)
+        io.emit('message', message);
     });
+
 });
 
 http.listen(8080, () => console.log('listening on http://localhost:8080'));
